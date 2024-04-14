b870432873494d2750f5951d43b7fa45a65f4ba1
Ehsan-Tavan <tavan.ehsan@gmail.com>
2024-04-14 14:59:31 +0330

    [add] creating new train and test samples.

--- src/prepare_training_data.py
+++ src/prepare_training_data.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 # ============================ My packages ============================
 from src.configuration import BaseConfig
 from src.data_loader import load_jsonl, save_to_jsonl
-from src.data_preparation import create_samples
+from src.data_preparation import create_samples, create_single_samples
 
 if __name__ == "__main__":
     CONFIG_CLASS = BaseConfig()
@@ -31,8 +31,21 @@ if __name__ == "__main__":
 
     TRAIN_DATA, DEV_DATA = train_test_split(SAMPLES, test_size=0.2, random_state=1234)
 
-    print(f"We have {len(TRAIN_DATA)} train data")
-    print(f"We have {len(DEV_DATA)} dev data")
+    SINGLE_TRAIN_DATA = create_single_samples(TRAIN_DATA)
+    SINGLE_DEV_DATA = create_single_samples(DEV_DATA)
 
-    save_to_jsonl(data=TRAIN_DATA, file_path=os.path.join(ARGS.processed_data_dir, ARGS.train_file))
-    save_to_jsonl(data=DEV_DATA, file_path=os.path.join(ARGS.processed_data_dir, ARGS.dev_file))
+    print(f"We have {len(TRAIN_DATA)}  pair samples train data")
+    print(f"We have {len(DEV_DATA)} pair samples dev data")
+
+    print(f"We have {len(SINGLE_TRAIN_DATA)}  single samples train data")
+    print(f"We have {len(SINGLE_DEV_DATA)} single samples dev data")
+
+    save_to_jsonl(data=TRAIN_DATA,
+                  file_path=os.path.join(ARGS.processed_data_dir, ARGS.pair_train_file))
+    save_to_jsonl(data=DEV_DATA,
+                  file_path=os.path.join(ARGS.processed_data_dir, ARGS.pair_dev_file))
+
+    save_to_jsonl(data=SINGLE_TRAIN_DATA,
+                  file_path=os.path.join(ARGS.processed_data_dir, ARGS.single_train_file))
+    save_to_jsonl(data=SINGLE_DEV_DATA,
+                  file_path=os.path.join(ARGS.processed_data_dir, ARGS.single_dev_file))
