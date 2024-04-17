ddf90c9cb895c95d3d82d8503b261fc50a58c295
Ehsan-Tavan <tavan.ehsan@gmail.com>
2024-04-17 18:00:34 +0330

    [add] binoculars package is added.

---
+++ src/binoculars/utils.py
@@ -0,0 +1,17 @@
+"""
+    Generative_AI_Authorship_Verification Project:
+        binoculars:
+            utils.py
+"""
+# ============================ Third Party libs ============================
+
+from transformers import AutoTokenizer
+
+
+def assert_tokenizer_consistency(model_id_1, model_id_2):
+    identical_tokenizers = (
+            AutoTokenizer.from_pretrained(model_id_1).vocab
+            == AutoTokenizer.from_pretrained(model_id_2).vocab
+    )
+    if not identical_tokenizers:
+        raise ValueError(f"Tokenizers are not identical for {model_id_1} and {model_id_2}.")
