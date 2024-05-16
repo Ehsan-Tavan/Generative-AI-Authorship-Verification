8d320ae4035f95b5c0e47807cd0e225b1c33bdcd
Ehsan-Tavan <tavan.ehsan@gmail.com>
2024-05-16 12:28:00 +0330

    [add] instruction_tuning_data_creator is added.

--- src/data_preparation/__init__.py
+++ src/data_preparation/__init__.py
@@ -4,5 +4,5 @@
 """
 from .data_preparation import create_samples, sequence_classification_data_creator, \
     create_single_samples, paraphraser_data_creator, generation_data_creator, \
-    sequence_classification_data_creator2
+    sequence_classification_data_creator2, instruction_tuning_data_creator
 from .training_data_preparation import prepare_data, create_paraphraser_data
